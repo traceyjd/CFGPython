2115b32b1c6766a97210a14044e992ee09b4cc87
Tracey <tracey.tdoherty@gmail.com>
2023-03-03 17:03:28 +0000

    New project for CFG

---
+++ CFGPython2.py
@@ -0,0 +1,12 @@
+#  Mutable arguments
+
+def replace_contents(g):
+    g[0] = 17
+    g[1] = 28
+    g[2] = 45
+    print('g =', g)
+
+f = [14, 23, 37]
+replace_contents(f)
+g = [17, 28, 45]
+print(f)
